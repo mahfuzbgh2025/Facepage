c846ab5d4cf82e87e363d98219f5358cc453009f
mahfuzbgh2025 <mahfuzbgh2025@gmail.com>
2025-08-30 12:34:43 +0600

    Create backend/src/models/User.js

---
+++ backend/backend/src/models/User.js
@@ -0,0 +1,12 @@
+cat > backend/src/models/User.js <<'EOF'
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profilePic: { type: String, default: "" }
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", userSchema);
+EOF
